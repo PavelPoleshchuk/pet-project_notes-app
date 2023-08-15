7ce8bf14c823cbbab6d08fc12f92b11afb0d19d9
PavelPoleshchuk <ppl@vivanta.by>
2023-08-15 19:01:27 +0300

    feat: add finde mode

--- my-tags/src/components/pages/Notes.tsx
+++ my-tags/src/components/pages/Notes.tsx
@@ -1,24 +1,60 @@
-import * as React from 'react';
-import {CiSearch} from 'react-icons/ci'
-import {BsPlusLg} from 'react-icons/bs'
-import { NoteItem } from '../NoteItem';
-import { Link } from 'react-router-dom';
-import { INoteItem } from '../../data_notes';
+import * as React from "react";
+import { CiSearch } from "react-icons/ci";
+import { BsPlusLg } from "react-icons/bs";
+import { NoteItem } from "../NoteItem";
+import { Link } from "react-router-dom";
+import { INoteItem } from "../../data_notes";
+import { useEffect, useState } from "react";
 type IProps = {
-  notes:INoteItem[]
+  notes: INoteItem[];
 };
-export const Notes = ({notes}:IProps) => {
+export const Notes = ({ notes }: IProps) => {
+  const [showSearch, setShowSearch] = React.useState(false);
+  const [title, setTitle] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState(notes);
+  console.log("filteredNotes=", filteredNotes);
+
+  const handleSearch = () => {
+    setFilteredNotes(
+      notes.filter((item) => {
+        if (item.title.toLowerCase().match(title.toLocaleLowerCase())) {return item}
+      })
+    );
+  };
+  useEffect(handleSearch,[title])
+
   return (
     <section>
       <header className="notes__header">
-        <h2>My notes</h2>
-        <input type="text" autoFocus placeholder='Keyword...'/>
-        <button className='notes__btn'><CiSearch/></button>
+        {!showSearch && <h2>My notes</h2>}
+        {showSearch && (
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.currentTarget.value);
+            }}
+            type="text"
+            autoFocus
+            placeholder="Keyword..."
+          />
+        )}
+        <button
+          onClick={() => {
+            setShowSearch((prev) => !prev);
+          }}
+          className="btn notes__btn"
+        >
+          <CiSearch />
+        </button>
       </header>
-      <main className='notes__container'>
-        {notes.map(item=><NoteItem key={item.id} item={item}/>)}
+      <main className="notes__container">
+        {filteredNotes.map((item) => (
+          <NoteItem key={item.id} item={item} />
+        ))}
       </main>
-      <Link to='/create-note' className='btn btn__plus'><BsPlusLg/></Link>
+      <Link to="/create-note" className="btn btn__plus">
+        <BsPlusLg />
+      </Link>
     </section>
   );
 };
